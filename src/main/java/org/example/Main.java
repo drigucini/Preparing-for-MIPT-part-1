67ad77f2d2017bac7e00ccb00db6677e08fcf8b1
Sergei Driga <drigaserge@gmail.com>
2023-06-05 17:23:20 +0300

    hw_done3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -95,10 +95,14 @@ public class Main {
         int determinant = b ^ 2 - 4 * a * c;
 
 
-        if (determinant >= 0) {
+        if (determinant > 0) {
             root1 = (-b + sqrt(determinant)) / (2 * a);
             root2 = (-b - sqrt(determinant)) / (2 * a);
-            System.out.println("The roots are: " + root1 + " and " + root2);
+            System.out.println("The roots are " + root2 + ", and " + root1);
+        } else if (determinant == 0) {
+            root2 = (-b + sqrt(determinant)) / (2 * a);
+            System.out.println("The root is " + root2);
+
         } else {
             System.out.println("The roots are irrational");
         }
@@ -145,46 +149,48 @@ public class Main {
         int secondDigit = number%10;
         System.out.println(tensNames[firstDigit] + " " + numberNames[secondDigit]);
 
-        switch (number) {
-            case (10):
-                System.out.println("ten");
-                break;
-            case (11):
-                System.out.println("eleven");
-                break;
-            case (12):
-                System.out.println("twelve");
-                break;
-            case (13):
-                System.out.println("thirteen");
-                break;
-            case (14):
-                System.out.println("fourteen");
-                break;
-            case (15):
-                System.out.println("fifteen");
-                break;
-            case (16):
-                System.out.println("sixteen");
-                break;
-            case (17):
-                System.out.println("seventeen");
-                break;
-            case (18):
-                System.out.println("eighteen");
-                break;
-            case (19):
-                System.out.println("nineteen");
-                break;
+        if (number >= 10 && number < 20) {
+            switch (number) {
+                case (10):
+                    System.out.println("ten");
+                    break;
+                case (11):
+                    System.out.println("eleven");
+                    break;
+                case (12):
+                    System.out.println("twelve");
+                    break;
+                case (13):
+                    System.out.println("thirteen");
+                    break;
+                case (14):
+                    System.out.println("fourteen");
+                    break;
+                case (15):
+                    System.out.println("fifteen");
+                    break;
+                case (16):
+                    System.out.println("sixteen");
+                    break;
+                case (17):
+                    System.out.println("seventeen");
+                    break;
+                case (18):
+                    System.out.println("eighteen");
+                    break;
+                case (19):
+                    System.out.println("nineteen");
+                    break;
+            }
         }
     }
 
     public static void checkNumber(int number) throws NotDoubleDigitException {
-        if (number / 100 != 0 || (number > -10 && number < 10)) {
+        if (number < 10 || number >= 100) {
             throw new NotDoubleDigitException("Input a two digit number");
         }
-
     }
+
     public static void task6() {
         System.out.println("\nTask 6");
         Scanner scanner = new Scanner(System.in);
@@ -199,8 +205,10 @@ public class Main {
         System.out.println("Input y0:");
         int y0 = scanner.nextInt();
 
-        if ((x - x0) * (x - x0) + (y - y0) * (y - y0) < r * r) { //почему нельзя сделать r^2?
+        if ((x - x0) * (x - x0) + (y - y0) * (y - y0) < r * r) {
             System.out.println("The coordinate " + x + ", " + y + " is inside the circle");
+        } else if ((x - x0) * (x - x0) + (y - y0) * (y - y0) == r * r) {
+            System.out.println("The coordinate " + x + ", " + y + " is on the circle");
         } else {
             System.out.println("The coordinate " + x + ", " + y + " is outside the circle");
         }
@@ -209,34 +217,41 @@ public class Main {
     public static void task7() {
         System.out.println("\nTask 7");
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
+        var number = scanner.nextDouble();
         System.out.println(check(number));
     }
 
     private static boolean check(double number) {
-        // пока не сообразил
+        double a = number/100;
+        double b = number/1000;
+        double c = number/10000;
+        if (a == 0 || b == 0 || c == 0) {
+            return true;
+        }
         return false;
     }
     private static boolean check(int number) {
-        if (number / 100 != 0 && number / 1000 == 0) {
+        if (number / 100 != 0 && number / 1000 == 0) { //a
             int a = number/100;
             int b = number/10%10;
             int c = number%10;
             if (number * number == a*a*a + b*b*b + c*c*c) {
                 return true;
             }
-            if (a == b || a == c || b == c) {
+            if (a == b || a == c || b == c) { //c
                 return true;
             }
         }
 
-        if (number / 1000 != 0 && number / 10000 == 0) {
+        if (number / 1000 != 0 && number / 10000 == 0) { //b
             int a = number/1000;
             int b = number/100%10;
             int c = number/10%10;
             int d = number%10;
             return a + b == c + d;
         }
+
+
         return false;
     }
 }
